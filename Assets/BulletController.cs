e91cac8253a6fde7dddb5f1835721ba9bd0722f0
H A <hugoa3520@gmail.com>
2022-03-20 01:57:58 +1100

    Generators shoot at player
    Bullets are shot once every 4 seconds and reach the bottom of the screen in 2 seconds.
    If i forgot to ask the team about how this messes with aspect ratio/resolution please remind me.

---
+++ Assets/BulletController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField]
+    private float fallSpeed = 2, homingSpeed = 2;
+    
+    private Transform player;
+
+    private float border;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        border = Camera.main.orthographicSize;
+
+        fallSpeed = (transform.position.y - (Camera.main.transform.position.y - border)) / fallSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _homing = Mathf.Sign(transform.position.x - player.position.x);
+        transform.position += new Vector3(_homing * -homingSpeed, -fallSpeed) * Time.deltaTime;
+    }
+    
+    void LateUpdate()
+    {
+        if(Mathf.Abs(transform.position.y) > border) Destroy(gameObject);
+    }
+
+    protected virtual void UpdateScores(string otherTag)
+    {
+        if(otherTag == "Player")
+        {
+            //player health--
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        UpdateScores(other.tag);
+        Destroy(gameObject);
+    }
+}
