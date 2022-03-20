87d0c255897f926dc22a912f81327953038a6bd2
TristanTran <13917733@student.uts.edu.au>
2022-03-20 19:04:09 +1100

    Player Movement
    Four way movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField]
+    public float moveSpeedX = 5f;
+    [SerializeField]
+    public float moveSpeedY = 3f;
+
+    private bool hasShotBullet;
+    private Vector2 movement;
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();    
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
     }
+
+	private void FixedUpdate() {
+        rb.MovePosition(rb.position + new Vector2(movement.x * moveSpeedX, movement.y * moveSpeedY) * Time.fixedDeltaTime);
+	}
 }
