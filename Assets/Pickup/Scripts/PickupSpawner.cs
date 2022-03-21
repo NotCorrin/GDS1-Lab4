16414a7824dddc4b6be9b33e549bb8e1970cfc36
BillingtonBigCat <wmajor21@gmail.com>
2022-03-21 23:45:23 +1100

    Spawner started

--- Assets/Pickup/Scripts/PickupSpawner.cs
+++ Assets/Pickup/Scripts/PickupSpawner.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickupManager : MonoBehaviour
+public class PickupSpawner : MonoBehaviour
 {
+    private float Timer;
+    private float PickupSpawnTime;
+
+    public GameObject spawnArea;
+    public GameObject pickup;
+    private BoxCollider2D polygonCollider;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        polygonCollider = spawnArea.GetComponent<BoxCollider2D>();
+        PickupSpawnTime = Random.Range(1, 3);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Timer += Time.deltaTime;
+
+        if (Timer > PickupSpawnTime /*&& !PickupExists*/)
+        {
+            Debug.Log("Spawned");
+            Timer = 0;
+            PickupSpawnTime = Random.Range(1, 3);
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        Vector2 rndPoint2D = RandomPointInBounds(polygonCollider.bounds, 1f);
+        Vector2 rndPointInside = polygonCollider.ClosestPoint(new Vector2(rndPoint2D.x, rndPoint2D.y));
+        if (rndPointInside.x == rndPoint2D.x && rndPointInside.y == rndPoint2D.y)
+        {
+            Instantiate(pickup, rndPointInside, Quaternion.identity);
+        }
+    }
+
+    private Vector2 RandomPointInBounds(Bounds bounds, float scale)
+    {
+        return new Vector3(
+            Random.Range(bounds.min.x * scale, bounds.max.x * scale),
+            Random.Range(bounds.min.y * scale, bounds.max.y * scale)
+        );
     }
 }
